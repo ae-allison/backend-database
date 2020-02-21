22bdc6e10ca53e77ad9e7c54d9b4222dec25b3b1
Anson Ervin <ansonervin@yahoo.com>
2020-02-21 09:29:58 -0500

    Added 2 more tests Added conditional to ensure database was initialized correctly

--- index.ts
+++ index.ts
@@ -1,55 +1,47 @@
-import admin from 'firebase-admin'
 import { DocumentData } from '@google-cloud/firestore';
 
-admin.initializeApp({
-  credential: admin.credential.cert('./config/firebaseAdminKey.json')
-});
-
-const db = admin.firestore();
- 
 type RECORD = {
   id: string
 }
 
-export class Database {
-  rootCollection: FirebaseFirestore.CollectionReference<DocumentData> | FirebaseFirestore.CollectionReference<DocumentData>;
-  initialRoot: FirebaseFirestore.CollectionReference<DocumentData> | FirebaseFirestore.CollectionReference<DocumentData>;
-  constructor(collection: string){
-    this.rootCollection = db.collection(collection)
-    this.initialRoot = db.collection(collection)
+type PATH = { 
+  firstCollection: string, 
+  doc: string, 
+  secondCollection: string 
+}
+
+export class AE_Allision {
+  rootCollection: FirebaseFirestore.CollectionReference<DocumentData> 
+  initialRoot: FirebaseFirestore.CollectionReference<DocumentData>
+
+  constructor(db: FirebaseFirestore.Firestore , collection?: string, path?: PATH){
+    if(!db || !collection && !path) throw("DB needs a collection or array of path to initialize.")
+    if(path){
+      const { firstCollection, doc, secondCollection } = path
+      this.rootCollection = db.collection(firstCollection).doc(doc).collection(secondCollection)
+      this.initialRoot = db.collection(firstCollection).doc(doc).collection(secondCollection)
+    } else {
+      this.rootCollection = db.collection(collection)
+      this.initialRoot = db.collection(collection)
+    }
   }
 
-  // public addToRootPath(collection: string, doc: string){
-  //   this.rootCollection = this.rootCollection.doc(doc).collection(collection)
-  // }
-
-  // public resetRootPath(initialRoot?: string){
-  //   initialRoot ? this.rootCollection = db.collection(initialRoot) : this.rootCollection = this.initialRoot
-  // }
-
-  public findOne(key: string, value: string): Promise<DocumentData> {
-    return new Promise((resolve, reject) => {
-      this.rootCollection.where(`${key}`, '==', value).get()
+  public async findOne(key: string, value: string): Promise<void |DocumentData> {
+      return this.rootCollection.where(`${key}`, '==', value)
+      .get()  
       .then(snapshot => {
         if (snapshot.empty) {
-          reject('No matching documents.');
+          return Promise.reject('No matching documents.');
         }  
-    
-        snapshot.forEach(doc => {
-          resolve(doc.data())
-        });
-      })
-      .catch(err => {
-        console.log('Error getting documents', err);
-      })
-    })
+        return Promise.resolve(snapshot.docs[0].data())
+      }, err => console.log('Error getting documents', err))
   }
 
   public findAll(): Promise<DocumentData[]> {
     return new Promise((resolve, reject) => {
       this.rootCollection
         .onSnapshot(snapshot => {
-          resolve(snapshot.docs.map(doc => doc.data()))
+          return resolve(snapshot.docs.map(doc => doc.data()))
         }, reject)
     })
   }
--- tests/db-spec.ts
+++ tests/db-spec.ts
@@ -1,9 +1,17 @@
 import 'mocha';
 import * as assert from 'assert';
 import sinon from 'sinon';
-import { Database } from '../'
+import { AE_Allision } from '../'
+import admin from 'firebase-admin'
 
-describe('Database class', () => {
+admin.initializeApp({
+    credential: admin.credential.cert('./config/firebaseAdminKey.json')
+  });
+  
+  const db = admin.firestore();
+ 
+  
+describe('AE_Allision class', () => {
     let sandbox;
 
     beforeEach(() => {
@@ -17,33 +25,37 @@ describe('Database class', () => {
         sandbox.restore();
     })
 
+    describe('Initializing db wrapper lib', () => {
+        it('it should fail because a collection or path was not passed', () => {
+           assert.throws(() => {
+               new AE_Allision(db)
+           })
+        })
+        it('it should initialize', () => {
+            new AE_Allision(db, 'test')
+         })
+    })
+
     describe('findAllDocuments', () => {
         it('it should find all documents', async () => {
-           const docs = await new Database('websites').findAll()
+           const docs = await new AE_Allision(db,'websites').findAll()
            const expectedDocuments = [
             { picture: 'grok.io', title: 'King Ju', name: 'kingju' }
            ]
            assert.deepEqual(docs[0], expectedDocuments[0]);
            return
         })
-        it('it should not find the documents', async () => {
-            assert.throws( ()=> {
-                new Database('').findAll().then(docs => {
-                    return 
-                })
-            })
-         })
     })
 
     describe('findOne', () => {
         it('it should find one document', async () => {
-           const docs = await new Database('websites').findOne('name', 'kingju')
-           const expectedDoc = { picture: 'grok.io', title: 'King Ju', name: 'kingju' }
+           const docs = await new AE_Allision(db,'websites').findOne('id', 'kingju')
+           const expectedDoc = { picture: 'grok.io', title: 'King Ju', id: 'kingju' }
            assert.deepEqual(docs, expectedDoc);
            return
         })
         it('it should not find the document', async () => {
-            await new Database('websites').findOne('name', 'f').then(docs => {
+            await new AE_Allision(db,'websites').findOne('name', 'f').then(docs => {
             }, err => {
                 assert.equal(err, 'No matching documents.')
             })
@@ -54,7 +66,7 @@ describe('Database class', () => {
         it('it should create one document', async () => {
            const doc = { picture: 'grok.io', title: 'King Ju', id: 'kingju' }
            try {
-               await new Database('websites').createAndUpdateOne(doc)
+               await new AE_Allision(db,'websites').createAndUpdateOne(doc)
                return 
            } catch(err){
                return err
